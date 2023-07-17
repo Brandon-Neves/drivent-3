17c26a1dac1f8234c389d3f9788ad574d70f496d
Brandon-Neves <84950033+Brandon-Neves@users.noreply.github.com>
2023-07-17 06:25:16 -0300

    fix

---
+++ hotels-controller.ts
@@ -0,0 +1,35 @@
+import { AuthenticatedRequest } from '@/middlewares';
+import hotelsServices from '@/services/hotels-service';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+
+export async function getHotels(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+
+  try {
+    const hotels = await hotelsServices.getHotels(Number(userId));
+    return res.status(httpStatus.OK).send(hotels);
+  } catch (error) {
+    if (error.name === 'NotFoundError') {
+      return res.status(httpStatus.NOT_FOUND);
+    }
+    return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+  }
+}
+
+export async function getHotelsRooms(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const { hotelId } = req.params;
+  try {
+    const hotels = await hotelsServices.getHotelsRooms(Number(userId), Number(hotelId));
+    return res.status(httpStatus.OK).send(hotels);
+  } catch (error) {
+    if (error.name === 'NotFoundError') {
+      return res.status(httpStatus.NOT_FOUND);
+    }
+    if (error.name === 'HotelsError') {
+      return res.status(httpStatus.PAYMENT_REQUIRED);
+    }
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+}
